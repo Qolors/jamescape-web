84c06bd75dc5d9af820cfecfd11da05a925fb6aa
qolors <cmickyg@gmail.com>
2022-09-14 13:04:06 -0400

    Formatting Fixed on posts page and independent posts

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -4,10 +4,10 @@ import { prisma } from "../server/db/client";
 const Posts: NextPage = (props: any) => {
 
   return (
-      <div className="w-full flex flex-col items-center justify-center min-h-screen">
+      <div className="w-full bg-base-200 pb-24 pt-24 flex flex-col items-center justify-center min-h-screen">
         
         <p className="text-2xl text-gray-700">Posts:</p>
-        <div className="w-full grid grid-cols-1 px-2 gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
+        <div className="w-full grid grid-cols-1 px-2 gap-6 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
           {props.posts.map((p: any) =>
            
           <a key={p.id} href={`/posts/${p.id}`}>
@@ -41,8 +41,10 @@ const TechnologyCard = ({
 }: TechnologyCardProps) => {
   return (
 
-      <div className="mx-auto flex w-full flex-col justify-center bg-base-100 rounded-2xl shadow-xl shadow-gray-400/20">
-        <img className="aspect-video max-w-full rounded-t-2xl object-contain object-center" src={`${image}`} />
+      <div className="mx-auto flex w-full object-cover flex-col justify-center bg-base-100 rounded-2xl shadow-xl shadow-gray-400/20">
+        <div className="h-[300px] overflow-hidden">
+          <img className="w-full rounded-t-2xl object-center object-cover h-[300px]" src={`${image}`} />
+        </div>
         <div className="p-6">
           <small className=" text-green-600 text-xs">{category}</small>
           <h1 className="text-2xl font-medium text-gray-800 pb-2">{name}</h1>
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -7,21 +7,22 @@ const Post = (props: any) => {
         <section className="bg-base-200 min-h-screen flex place-items-center">
             <div className="container px-6 py-10 mx-auto">
                 <div className="lg:-mx-6 lg:flex lg:items-center">
-                    <img className="object-cover object-center lg:w-1/2 lg:mx-6 w-full h-96 rounded-lg lg:h-[36rem]" src={props.post.image !== '' ? `${props.post.image}` : '../jamescape.png'} alt="" />
+                    <img className="object-contain object-center lg:w-1/2 lg:mx-6 w-full h-96 rounded-lg lg:h-[36rem]" src={props.post.image !== '' ? `${props.post.image}` : '../jamescape.png'} alt="" />
 
-                    <div className="mt-8 lg:w-1/2 lg:px-6 lg:mt-0">
+                    <div className="mt-8 lg:w-1/2 lg:px-6 lg:mt-0 pb-24">
                         <p className="text-5xl font-semibold text-primary ">“</p>
 
                         <h1 className="text-2xl font-semibold text-primary xl:text-4xl lg:w-96">
                             {props.post.title}
                         </h1>
+                        <h3 className="mt-6 text-lg font-medium text-primary">{props.post.category}</h3>
+                        <p className="text-secondary dark:text-gray-300">legault432</p>
 
                         <p className="max-w-lg mt-6 text-gray-800">
                             {props.post.body}
                         </p>
 
-                        <h3 className="mt-6 text-lg font-medium text-primary">{props.post.category}</h3>
-                        <p className="text-secondary dark:text-gray-300">legault432</p>
+                        
                     </div>
                 </div>
             </div>
