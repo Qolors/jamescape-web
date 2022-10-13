d9a1d4e3d20f1f6c344d08b7a5030aeb4012ab4c
qolors <cmickyg@gmail.com>
2022-10-12 22:27:33 -0400

    Fixed Skill Order and Data

--- src/components/skilling.tsx
+++ src/components/skilling.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 const Skilling = (props: any) => {
 
-    const [skills, setSkills] = useState<Object>([])
+    const [skills, setSkills] = useState<any>([])
     
     useEffect(() => {
         const x = [];
